259411989f8587d28df5257430c5c649eae0b205
MaFalana <malikfalana@icloud.com>
2025-09-16 15:30:41 -0400

    Update base path for /breakline-gen subdirectory deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   envPrefix: 'VITE_',
-  base: '/',
+  base: '/breakline-gen/',
   build: {
     assetsDir: 'assets',
   },
